ad337cdd4126af4f2f94b26f9a3d5d8a1dca537f
boosb <boosb@mail.ru>
2024-08-09 12:45:22 +0300

    added origin in the enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "http://46.19.67.196", // todo точка правки для docker (порт: 8080/4200)
+    "origin": ["http://46.19.67.196", "http://dailydestin.ru"],
     "credentials": true,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
