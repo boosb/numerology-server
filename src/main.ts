4b3a9c606ce1fd32f90934880aedaa7ed1f3b26f
boosb <boosb@mail.ru>
2024-08-09 17:12:23 +0300

    change enableCors function

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "http://46.19.67.196, http://dailydestin.ru",
+    "origin": true,
     "credentials": true,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
