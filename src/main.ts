543003de7d899e4e7c5457df2b6b19187bef6324
boosb <boosb@mail.ru>
2024-08-09 18:02:10 +0300

    change main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
+  const app = await NestFactory.create(AppModule, {cors: true});
+  /*app.enableCors({
     "origin": true,
     "credentials": true,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
-  });
+  });*/
   app.use(cookieParser());
   await app.listen(3000);
 }
