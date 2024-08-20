22984a0d560c7d237b49e0e0e47975b3e8cd4cda
boosb <boosb@mail.ru>
2024-08-20 16:40:28 +0300

    bug fix with CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    "origin": "http://dailydestin.ru:80",
+    "credentials": true,
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
   app.use(cookieParser());
   await app.listen(3000);
 }
