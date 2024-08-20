d1386acb651633573facfe30efbf905f3fb5a838
boosb <boosb@mail.ru>
2024-08-20 18:21:58 +0300

    bug fix commit

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     //"origin": false 
-    "origin": "http://dailydestin.ru:80/",
+    "origin": ["http://dailydestin.ru:80/", "http://dailydestin.ru:5050/", "http://dailydestin.ru:5432/"],
     "credentials": true,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
